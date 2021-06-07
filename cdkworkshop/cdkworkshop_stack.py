f8835c9ba356b09bbe12fa832c3f0cab090a5716
Christoph Klingspor <klingspor@gmx.net>
2021-06-07 23:21:58 +0200

    Finished 'Hello CDK' section of the tutorial: https://cdkworkshop.com/30-python/40-hit-counter.html

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     core,
-    aws_lambda as _lambda #cant use lambda here because its a python element
-
+    aws_lambda as _lambda, #cant use lambda here because its a python element
+    aws_apigateway as apigw,
 )
 
 
@@ -11,10 +11,11 @@ class CdkworkshopStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # the id of the construct (here lambda) must be unique within this scope
+        #self?
         function = _lambda.Function(self,
                                     id='HelloFromCDK',
                                     runtime=_lambda.Runtime.PYTHON_3_7,
                                     code=_lambda.Code.asset('lambda'), # same as dir of lambda code
                                     handler='hello_from_cdk.handler', #filename.handlername
                                     )
-
+        gateway = apigw.LambdaRestApi(self, id='CDKendpoint', handler=function)
